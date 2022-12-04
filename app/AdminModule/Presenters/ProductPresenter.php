bc54d3fd1f62e49e8178f050ef7af92f3ffd1ed4
lukas-vit <71551100+lukas-vit@users.noreply.github.com>
2022-12-04 22:26:21 +0100

    přidání editace produktů

---
+++ app/AdminModule/Presenters/ProductPresenter.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use App\AdminModule\Components\ProductEditForm\ProductEditForm;
+use App\AdminModule\Components\ProductEditForm\ProductEditFormFactory;
+use App\Model\Facades\ProductsFacade;
+
+/**
+ * Class ProductPresenter
+ * @package App\AdminModule\Presenters
+ */
+class ProductPresenter extends BasePresenter{
+  /** @var ProductsFacade $productsFacade */
+  private $productsFacade;
+  /** @var ProductEditFormFactory $productEditFormFactory */
+  private $productEditFormFactory;
+
+  /**
+   * Akce pro vykreslení seznamu produktů
+   */
+  public function renderDefault():void {
+    $this->template->products=$this->productsFacade->findProducts(['order'=>'title']);
+  }
+
+  /**
+   * Akce pro úpravu jednoho produktu
+   * @param int $id
+   * @throws \Nette\Application\AbortException
+   */
+  public function renderEdit(int $id):void {
+    try{
+      $product=$this->productsFacade->getProduct($id);
+    }catch (\Exception $e){
+      $this->flashMessage('Požadovaný produkt nebyl nalezen.', 'error');
+      $this->redirect('default');
+    }
+    $form=$this->getComponent('productEditForm');
+    $form->setDefaults($product);
+    $this->template->product=$product;
+  }
+
+  /**
+   * Akce pro smazání produktu
+   * @param int $id
+   * @throws \Nette\Application\AbortException
+   */
+  public function actionDelete(int $id):void {
+    try{
+      $product=$this->productsFacade->getProduct($id);
+    }catch (\Exception $e){
+      $this->flashMessage('Požadovaná kategorie nebyla nalezena.', 'error');
+      $this->redirect('default');
+    }
+
+    if (!$this->user->isAllowed($product,'delete')){
+      $this->flashMessage('Tuto kategorii není možné smazat.', 'error');
+      $this->redirect('default');
+    }
+
+    if ($this->productsFacade->deleteProduct($product)){
+      $this->flashMessage('Produkt byl smazán.', 'info');
+    }else{
+      $this->flashMessage('Tento produkt není možné smazat.', 'error');
+    }
+
+    $this->redirect('default');
+  }
+
+  /**
+   * Formulář na editaci produktů
+   * @return ProductEditForm
+   */
+  public function createComponentProductEditForm():ProductEditForm {
+    $form = $this->productEditFormFactory->create();
+    $form->onCancel[]=function(){
+      $this->redirect('default');
+    };
+    $form->onFinished[]=function($message=null){
+      if (!empty($message)){
+        $this->flashMessage($message);
+      }
+      $this->redirect('default');
+    };
+    $form->onFailed[]=function($message=null){
+      if (!empty($message)){
+        $this->flashMessage($message,'error');
+      }
+      $this->redirect('default');
+    };
+    return $form;
+  }
+
+  #region injections
+  public function injectProductsFacade(ProductsFacade $productsFacade){
+    $this->productsFacade=$productsFacade;
+  }
+  public function injectProductEditFormFactory(ProductEditFormFactory $productEditFormFactory){
+    $this->productEditFormFactory=$productEditFormFactory;
+  }
+  #endregion injections
+
+}
